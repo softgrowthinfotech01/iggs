efca874c222fe3f6086f5c6f2026b9903efb808d
Varun-9997 <dangrevarun@gmail.com>
2026-05-29 17:13:47 +0530

    cd

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -39,7 +39,7 @@ $totalSliders = $sliderQuery->fetchColumn();
     class=" relative pt-20 lg:pl-60 bg-slate-100 transition-all duration-300 min-h-screen flex flex-col opacity-0 translate-y-6 scale-[0.98] ease-out">
 
      <!-- Background Logo -->
-    <div class="absolute inset-0 pointer-events-none z-0"
+    <div class="absolute inset-0 pointer-events-none z-0"  
         style="
             background-image:url('images/1779876371_IG_logo_transparent.png');
             background-repeat:no-repeat;
